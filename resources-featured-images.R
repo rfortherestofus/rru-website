7f8efae800beddb60a7034928c8c9e8124c77405
Thomas Vroylandt <tvroylandt@gmail.com>
2021-02-10 21:56:52 +0100

    Function to center logo

--- resources-featured-images.R
+++ resources-featured-images.R
@@ -4,17 +4,25 @@
 
 library(tidyverse)
 library(janitor)
+library(magick)
+library(rsvg)
 
-# Screenshot --------------------------------------------------------------
+# Import ------------------------------------------------------------------
 # list of resources + comparing to missing - delete the screenshot to generate a new one
-resources <- read_csv("data/Resources-For export.csv") %>%
+# from Airtable
+resources <- read_csv("data/Resources-Grid view.csv") %>%
   clean_names() %>%
-  mutate(filename = paste0("plots/", make_clean_names(title, sep_out = "-"), ".png")) %>% 
+  mutate(filename = paste0("plots/", make_clean_names(title, sep_out = "-"), ".png")) %>%
   filter(!filename %in% list.files("plots", full.names = TRUE))
 
+# Screenshot --------------------------------------------------------------
+# filtering on no logo
+resources_to_screenshot <- resources %>%
+  filter(is.na(logo))
+
 # screenshot and resizing - webshot is vectorized but it generates some errors
-walk2(resources$url,
-      resources$filename,
+walk2(resources_to_screenshot$url,
+      resources_to_screenshot$filename,
       function(url, filename) {
         webshot2::webshot(
           url = url,
@@ -27,6 +35,37 @@ walk2(resources$url,
           webshot::resize("400x")
       })
 
+# Logo --------------------------------------------------------------------
+# filtering on logo
+resources_to_logo <- resources %>%
+  filter(!is.na(logo))
+
+# loading a white square
+white_square <-
+  image_read_svg("https://upload.wikimedia.org/wikipedia/commons/1/1f/Blank_square.svg") %>%
+  image_crop("212x212+50")
+
+# download, resize, center, save
+walk2(
+  resources_to_logo$logo,
+  resources_to_logo$filename,
+  possibly(function(logo, filename) {
+    if (str_detect(logo, ".svg")) {
+      logo_img <- image_read_svg(logo) %>%
+        image_scale("400x")
+    } else {
+      logo_img <- image_read(logo) %>%
+        image_scale("400x")
+    }
+    
+    image_append(c(white_square, logo_img, white_square)) %>%
+      image_scale("400x225") %>%
+      image_transparent("white") %>%
+      image_background("white") %>%
+      image_write(filename)
+  }, otherwise = NULL)
+)
+
 # Create spreadsheet ------------------------------------------------------
 resources_featured_images <- resources %>%
   mutate(
@@ -36,5 +75,5 @@ resources_featured_images <- resources %>%
       ".png"
     )
   ) %>%
-  select(title, featured_image) %>% 
+  select(title, featured_image) %>%
   write_csv("data/Resources-screenshot_names.csv")
